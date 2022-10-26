e67b3ca64635a1be5d7c8dbb7f28d03acd988689
Matthew Miller <mwm@fastmail.com>
2022-10-26 14:09:56 +0200

    Increment version

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule Baobab.MixProject do
   def project do
     [
       app: :baobab,
-      version: "0.12.1",
+      version: "0.13.0",
       elixir: "~> 1.13",
       name: "Baobab",
       source_url: "https://github.com/mwmiller/baobab_ex",
